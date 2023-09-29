391cc8ef50de3672d37a17395d674c6185174fbc
dieginin <diegobalestra9@gmail.com>
2023-09-29 15:51:59 -0400

    return str instead of int

--- functions/cogload.py
+++ functions/cogload.py
@@ -3,16 +3,16 @@ import os
 from discord.ext import commands
 
 
-async def cogload(bot: commands.Bot) -> int:
-    cogs_loaded = 0
+async def cogload(bot: commands.Bot) -> str:
+    cogs_loaded = []
 
     for folder in ["commands", "handlers"]:
         for filename in os.listdir(f"./{folder}"):
             if filename.endswith(".py"):
                 try:
                     await bot.load_extension(f"{folder}.{filename[:-3]}")
-                    cogs_loaded += 1
+                    cogs_loaded.append(f"{filename[:-3]}")
                 except commands.ExtensionError as e:
                     print(f"Error al cargar la extensión {filename}: {str(e)}")
 
-    return cogs_loaded
+    return ", ".join(cogs_loaded)
