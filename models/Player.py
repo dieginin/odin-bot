d086204478ec44099a5591f21e9d25c124a8b0ca
dieginin <diegobalestra9@gmail.com>
2023-09-29 13:34:32 -0400

    setting up player model

---
+++ models/Player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(
+        self,
+        _id: int,
+        pocket: int,
+        bank: int,
+    ):
+        self.id = _id
+        self.pocket = pocket
+        self.bank = bank
