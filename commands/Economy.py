706fa919715d4b12d7fafeb352ae068143ca95d6
dieginin <diegobalestra9@gmail.com>
2023-09-30 01:03:33 -0400

    earn command added

--- commands/Economy.py
+++ commands/Economy.py
@@ -1,10 +1,11 @@
+import random
 from typing import Optional
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-from functions import getplayer
+from functions import getplayer, playerload
 
 
 class Economy(commands.Cog):
@@ -30,6 +31,36 @@ class Economy(commands.Cog):
         em.add_field(name="👛 Bolsa", value=f"`{pl.pocket:^9,}`")
         em.add_field(name="🏦 Banco", value=f"`{pl.bank:^9,}`")
         em.add_field(name="🪙 V. Neto", value=f"`{pl.pocket + pl.bank:^9,}`")
+
+        await interaction.followup.send(embed=em)
+
+    @app_commands.checks.cooldown(2, 86400, key=lambda i: i.user.id)
+    @app_commands.command(description="Gana peniques dos veces cada dia")
+    async def ganar(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+
+        id = interaction.user.id
+        pl = playerload(id)
+        m = discord.utils.get(self.bot.get_all_members(), id=pl.id)
+
+        earns = random.randint(1, random.randint(50, 200))
+
+        pl.pocket += earns
+        em = discord.Embed()
+        em.set_thumbnail(
+            url="https://cdn-icons-png.flaticon.com/512/10749/10749511.png"
+        )
+        if m:
+            em.set_author(name=m.display_name, icon_url=m.display_avatar.url)
+
+        if bool(pl.save()):
+            em.color = discord.Color.green()
+            em.description = (
+                f"Ganaste 🪙 `{earns}`\nAhora tienes 👛 `{pl.pocket:,}` en la bolsa"
+            )
+        else:
+            em.color = discord.Color.dark_red()
+            em.description = f"Tuve un 💨\nNo pude guardar \nGanancia de 🪙 `{earns}`"
 
         await interaction.followup.send(embed=em)
 
