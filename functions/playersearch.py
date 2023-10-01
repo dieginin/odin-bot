f2d51a7b836583b6f4276c66914bee7694d44e66
dieginin <diegobalestra9@gmail.com>
2023-10-01 00:05:04 -0400

    migration to tools integration

--- functions/playersearch.py
+++ functions/playersearch.py
@@ -5,4 +5,8 @@ from models import Player
 def playersearch(id: int) -> Player:
     result = players.find_one({"_id": id})
 
-    return Player(**result) if result else Player(id, 0, 0)
+    return (
+        Player(**result)
+        if result
+        else Player(id, 0, 0, {"bow_and_arrow": 0, "rod": 0, "pick": 0})
+    )
