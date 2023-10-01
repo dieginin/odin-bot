970348a99a914d8aeb50c8e6f01ca5e233db4655
dieginin <diegobalestra9@gmail.com>
2023-10-01 18:10:34 -0400

    hunt command added

--- commands/Economy.py
+++ commands/Economy.py
@@ -6,8 +6,14 @@ from discord import app_commands
 from discord.ext import commands
 
 from components import EcoEmbed
-from errors import InsufficientBalance, InsufficientCoins, SecureCoins
+from errors import (
+    InsufficientBalance,
+    InsufficientCoins,
+    InsufficientResources,
+    SecureCoins,
+)
 from functions import getplayer, playerload
+from models import Animal
 
 
 class Economy(commands.Cog):
@@ -133,6 +139,35 @@ class Economy(commands.Cog):
         em.add_field(name="**☵ Animales**", value=animals_)
         em.add_field(name="**☵ Peces**", value="*Sin peces*")
         em.add_field(name="**☵ Reliquias**", value="*Sin reliquias*")
+
+        await interaction.followup.send(embed=em)
+
+    @app_commands.command(description="Utiliza tus arcos para cazar animales")
+    async def cazar(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+
+        id = interaction.user.id
+        pl = playerload(id)
+
+        if pl.tools["bow_and_arrow"] <= 0:
+            raise InsufficientResources("bow_and_arrow")
+
+        em = EcoEmbed(self.bot, pl.id)
+        em.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/3553/3553171.png")
+
+        pl.change_tools("bow_and_arrow", -1)
+        weights = [a().probability for a in Animal.__subclasses__()]  # type: ignore
+        animal = random.choices(Animal.__subclasses__(), weights=weights)[0]()  # type: ignore
+
+        caza = random.choices([0, 1], weights=[0.1, 0.9], k=1)[0]
+        if caza:
+            pl.change_animal(animal, 1)
+            em.color = discord.Color.green()
+            em.description = f"**Felicidades!**\nCazaste un :{animal.hash}: `{animal.name}`\nTienes `x{pl.animals[animal.hash]['quantity']:,} {animal.name}`"
+        else:
+            em.color = discord.Color.red()
+            em.description = f"**Oh no!**\nUn :{animal.hash}: `{animal.name}` se te escapó\nMejor suerte para la próxima"
+        pl.save()
 
         await interaction.followup.send(embed=em)
 
